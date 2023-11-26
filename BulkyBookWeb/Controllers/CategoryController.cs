1dc4125154b82111d717649238533e60661f824a
AmilaJayamaha <amilajayamaha96@gmail.com>
2023-11-26 20:17:14 +0530

    Added create button and added features

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -18,5 +18,19 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        //Get
+        public IActionResult Create()
+        {
+              return View();
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
